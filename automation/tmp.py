9cf441a3f18d7b0089915c4693c9a204da7387a0
rr <moc_11355@163.com>
2025-07-30 22:08:40 +0800

    write automation-tiktok

--- automation/tmp.py
+++ automation/tmp.py
@@ -1,37 +1,75 @@
+import sys
+import time
+
 from DrissionPage import Chromium
 
 
 class TikTok(object):
+
     def __init__(self):
         self.browser = Chromium().latest_tab
 
-
     def search(self, search_text: str):
+        """
+        抖音搜索
+        :param search_text: 搜索文本
+        :return:
+        """
         self.browser.get(url='https://www.douyin.com/')
-        button = self.browser.ele('css:input[placeholder="搜索你感兴趣的内容"]')
+        time.sleep(.9)
+        button = self.browser.ele('css:input[data-e2e="searchbar-input"][type="text"]')
         button.input(search_text)
-
         self.browser.ele('css:button[data-e2e="searchbar-button"][type="button"]').click()
 
-    def listen_pipeline(self, xhr: str = "XHR"):
+    def listen_start(self, xhr: str):
+        """
+        启动监听器
+        :param xhr: 监听路径
+        :return:
+        """
         self.browser.listen.start(xhr)
 
-    def set_search_where(self,selector,where):
-        items = {i.text:i for i in self.browser.eles('css:span[data-key]')}
+    def listen_wait(self):
+        res = self.browser.listen.wait()
+        print(res.response.body)
+        return
+
+    def set_search_where(self, selector, where: {str: str}):
+        """
+         修改搜索条件
+        :param selector: 【综合、视频、用户、直播】
+        :param where: 筛选条件
+        :return:
+        """
+        time.sleep(.9)
+        items = {i.text: i for i in self.browser.eles('css:span[data-key]')}
         if selector not in items.keys():
-            raise f"选项必须在 {items.keys()} "
+            print(f"选项必须在 {items.keys()} ")
+            sys.exit()
         items[selector].click()
 
-        # shaixuan 鼠标悬停 后选择 相关的标签。
-        # 开启 监听 分析数据包 然后将数据包相关数据 存储在 mydata.db 中
-
+        self.browser.ele('css:svg.arrow').hover()
+        for k, v in where.items():
+            _ = self.browser.ele(f'css:span[data-index1="{k}"][data-index2="{v}"]')
+            if _:
+                _.click()
+            else:
+                ...
 
         ...
 
 
-
 if __name__ == '__main__':
     cls = TikTok()
-    # cls.listen_pipeline('/aweme/v1/web/search/item/')
+    #
     # cls.search('乐陵影视城')
-    cls.set_search_where(None,None)
+    ll = {
+        '0': '1',
+        '1': '1',
+        '2': '0',
+        '3': '0',
+    }
+    time.sleep(.9)
+    cls.listen_start('/aweme/v1/web/search/item/')
+    cls.set_search_where('视频', ll)
+    cls.listen_wait()
